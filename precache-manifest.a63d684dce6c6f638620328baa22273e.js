20dee7cc7b06725e03abcfa46402dc43b99b32fc
Lukas Was <wetsand@o2.pl>
2020-10-22 22:44:49 +0200

    Updates

---
+++ precache-manifest.a63d684dce6c6f638620328baa22273e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310f4e668ecccff7420a4eca70add27f",
+    "url": "/ToDo-App/index.html"
+  },
+  {
+    "revision": "b72f216c10717e5e2d58",
+    "url": "/ToDo-App/static/css/main.57a56ead.chunk.css"
+  },
+  {
+    "revision": "8533f7c89e331bc63a0d",
+    "url": "/ToDo-App/static/js/2.435a1e0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDo-App/static/js/2.435a1e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b72f216c10717e5e2d58",
+    "url": "/ToDo-App/static/js/main.7d7b7cdd.chunk.js"
+  },
+  {
+    "revision": "8db22120fb0b75e7b05e",
+    "url": "/ToDo-App/static/js/runtime-main.7216bf10.js"
+  }
+]);
